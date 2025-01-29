c8aa0a9c8578614f43f862a8c2244e6910f0093e
Marcos <marcosvieira120404@gmail.com>
2025-01-29 15:36:29 -0300

    feat: bishop movement added

--- src/pieces/bishop.cpp
+++ src/pieces/bishop.cpp
@@ -1,12 +1,17 @@
 #include "bishop.h"
+#include <cmath>
+#include <iostream>
 
 PieceType Bishop::getType() const{
     return PieceType::Bishop;
 };
         
-bool Bishop::isValidMovement(int startX, int endX, int startY, int endY){
-
-};
+bool Bishop::isValidMovement(int startX, int startY, int endX, int endY) {
+    int deltaX = endX - startX;
+    int deltaY = endY - startY;
+    return abs(deltaX) == abs(deltaY);
+}
 
 bool Bishop::canEliminate(int startX, int startY, int endX, int endY, Colors targetColor){
+    return isValidMovement(startX, startY, endX, endY) && targetColor != this->color;
 }
