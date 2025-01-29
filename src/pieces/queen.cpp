3d5ae1af007da6af77cd424b3c6f07403e8d780d
Marcos <marcosvieira120404@gmail.com>
2025-01-29 15:37:19 -0300

    feat: queen movement added

--- src/pieces/queen.cpp
+++ src/pieces/queen.cpp
@@ -1,11 +1,14 @@
 #include "queen.h"
+#include <cmath>
 
 PieceType Queen::getType() const{
     return PieceType::Queen;
 }
 
-bool Queen::isValidMovement(int startX, int endX, int startY, int endY){
-
+bool Queen::isValidMovement(int startX, int startY, int endX, int endY){
+    int deltaX = endX - startX;
+    int deltaY = endY - startY;
+    return (startX == endX || startY == endY) || abs(deltaX) == abs(deltaY);
 }
 
 bool Queen::canEliminate(int startX, int startY, int endX, int endY, Colors targetColor){
